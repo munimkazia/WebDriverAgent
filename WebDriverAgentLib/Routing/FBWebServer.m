a1ebc981e88970cff36b6abccec014d9860d00ce
Abdul Munim kazia <munim@browserstack.com>
2017-07-06 19:39:34 +0530

    fixes

--- WebDriverAgentLib/Routing/FBWebServer.m
+++ WebDriverAgentLib/Routing/FBWebServer.m
@@ -28,7 +28,7 @@
 static NSString *const FBServerURLBeginMarker = @"ServerURLHere->";
 static NSString *const FBServerURLEndMarker = @"<-ServerURLHere";
 struct mg_mgr mgr;
-const char *tcp_port = "8200"
+const char *tcp_port = "8200";
 
 @interface FBHTTPConnection : RoutingConnection
 @end
@@ -73,7 +73,12 @@ const char *tcp_port = "8200"
   [self startHTTPServer];
   dispatch_async(dispatch_get_main_queue(),
   ^{
-    [self startTCPServer];    
+    mg_mgr_init(&mgr, NULL);
+    mg_bind(&mgr, tcp_port, ev_handler);
+    while (true) {
+      mg_mgr_poll(&mgr, 1000);
+    }
+    // mg_mgr_free(&mgr);
   });
   
   [[NSRunLoop mainRunLoop] run];
@@ -82,31 +87,21 @@ const char *tcp_port = "8200"
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
     struct mbuf *io = &nc->recv_mbuf;
     (void) p;
-
+    NSData *screenshot;
     switch (ev) {
       case MG_EV_RECV:
-      NSData *screenshot = [[XCAXClient_iOS sharedClient] screenshotData];
-      char start[1] = "b";
-      mg_send(nc, start, 1);
-      unsigned long imgdata_size = screenshot.length;
-      char *imageSizeBuf = (char*) &imgdata_size;
-      mg_send(nc,imageSizeBuf, sizeof(imgdata_size));
-      mg_send(nc, screenshot, screenshot.length);   // Echo message back
-      mbuf_remove(io, io->len);       // Discard message from recv buffer
+        screenshot = [[XCAXClient_iOS sharedClient] screenshotData];
+        char start[1] = "b";
+        mg_send(nc, start, 1);
+        unsigned long imgdata_size = screenshot.length;
+        char *imageSizeBuf = (char*) &imgdata_size;
+        mg_send(nc,imageSizeBuf, sizeof(imgdata_size));
+        mg_send(nc, screenshot.bytes, (int)screenshot.length);   // Echo message back
+        mbuf_remove(io, io->len);       // Discard message from recv buffer
       break;
     }
 }
 
-
-- (void)startTCPServer
-{
-  mg_mgr_init(&mgr, NULL);
-  mg_bind(&mgr, tcp_port, ev_handler);
-  for (;;) {
-    mg_mgr_poll(&mgr, 1000);
-  }
-  mg_mgr_free(&mgr);
-}
 
 - (void)startHTTPServer
 {
